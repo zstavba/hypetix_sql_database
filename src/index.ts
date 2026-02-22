2aed3ee442c9584125c49a4f9a3b7d7722d12f21
Žan Stavbar <46623220+zstavba@users.noreply.github.com>
2026-02-22 13:36:55 +0100

    port update

--- src/index.ts
+++ src/index.ts
@@ -171,7 +171,7 @@ AppDataSource.initialize()
       });
     });
 
-    server.listen(PORT, () => {
+    server.listen(PORT, Number('0.0.0.0'), () => {
       console.log(`Server running on port ${PORT}`);
     });
   });

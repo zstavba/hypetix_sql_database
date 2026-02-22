bea5eff56229d0d758dfdf219e8ffe342bcf40a8
Žan Stavbar <46623220+zstavba@users.noreply.github.com>
2026-02-22 17:43:49 +0100

    sync

--- src/data-source.ts
+++ src/data-source.ts
@@ -27,7 +27,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities: [
         User,
